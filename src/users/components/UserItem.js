193837a594162e86c52cbb550c06233693da9570
Daniel ng <meenereem@gmail.com>
2024-01-19 15:44:25 -0500

    card

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Card from "../../shared/components/UIElements/Card";
 import Avatar from "../../shared/components/UIElements/Avatar";
 import "./UserItem.css";
 
 const UsersItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <Link to={``}>
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
             <Avatar image={props.image} alt={props.name} />
           </div>
@@ -19,7 +20,7 @@ const UsersItem = (props) => {
             </h3>
           </div>
         </Link>
-      </div>
+      </Card>
     </li>
   );
 };
